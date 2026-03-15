50a6af79e66f30089b9b21c95a6988743c1ba66a
Priya Shrivastava <shrivastavapriya2001@gmail.com>
2026-03-15 13:28:03 +0530

    Created core apps : resources, billing, enrollments, dashboard

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resources',
+    'billing',
+    'enrollments',
+    'dashboard',
+ 
 ]
 
 MIDDLEWARE = [
